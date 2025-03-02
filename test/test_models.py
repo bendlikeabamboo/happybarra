0eb9239ffc543b20f198816d9f3ba6f6c4916c47
Manuel B. <mbalmeo32@gmail.com>
2025-03-02 14:23:08 +0800

    list of cc charges

--- test/test_models.py
+++ test/test_models.py
@@ -1,4 +1,11 @@
-from happybara.models import CreditCard, DueDateType, WeekendCheckPolicy
+from happybara.models import (
+    CreditCard,
+    DueDateType,
+    WeekEndPolicy,
+    CreditCardInstallment,
+    InstallmentPolicy,
+    InstallmentAmountType,
+)
 import pytest
 import datetime as dt
 
@@ -10,7 +17,7 @@ def fixture_cc():
         name="platinum",
         network="mastercard",
         due_date_type=DueDateType.X_DAYS_AFTER,
-        due_date_reference=1,
+        due_date_ref=1,
         statement_day=2,
     )
 
@@ -22,7 +29,7 @@ def fixture_cc_swe_dwd():
         name="platinum",
         network="mastercard",
         due_date_type=DueDateType.X_DAYS_AFTER,
-        due_date_reference=8,
+        due_date_ref=8,
         statement_day=2,
     )
 
@@ -30,59 +37,55 @@ def fixture_cc_swe_dwd():
 def test_next_statement_before_statement(cc: CreditCard):
     _input = dt.date(2025, 3, 1)
     expct = dt.date(2025, 4, 2)
-    actual = cc._next_statement(_input)
+    actual = cc.next_statement(_input)
     assert expct == actual
 
 
 def test_next_statement_on_statement(cc: CreditCard):
     expected = dt.date(2025, 3, 2)
-    actual = cc._next_statement(dt.date(2025, 3, 2))
+    actual = cc.next_statement(dt.date(2025, 3, 2))
     assert expected == actual
 
 
 def test_next_statement_aft_statement(cc: CreditCard):
     expected = dt.date(2025, 4, 2)
-    actual = cc._next_statement(dt.date(2025, 3, 3))
+    actual = cc.next_statement(dt.date(2025, 3, 3))
     assert expected == actual
 
 
 def test_weekend_check_prev_business_day(cc_swe_dwd: CreditCard):
     expected = dt.date(2025, 3, 7)
-    actual = cc_swe_dwd._weekend_check(
-        dt.date(2025, 3, 8), WeekendCheckPolicy.PREVIOUS_BUSINESS_DAY
-    )
+    actual = cc_swe_dwd._weekend_check(dt.date(2025, 3, 8), WeekEndPolicy.PREV_BANK_DAY)
     assert expected == actual
+
 
 def test_weekend_check_next_business_day(cc_swe_dwd: CreditCard):
     expected = dt.date(2025, 3, 10)
-    actual = cc_swe_dwd._weekend_check(
-        dt.date(2025, 3, 8), WeekendCheckPolicy.NEXT_BUSINESS_DAY
-    )
+    actual = cc_swe_dwd._weekend_check(dt.date(2025, 3, 8), WeekEndPolicy.NEXT_BANK_DAY)
     assert expected == actual
+
 
 def test_weekend_check_prev_business_day_sunday(cc_swe_dwd: CreditCard):
     expected = dt.date(2025, 3, 7)
-    actual = cc_swe_dwd._weekend_check(
-        dt.date(2025, 3, 9), WeekendCheckPolicy.PREVIOUS_BUSINESS_DAY
-    )
+    actual = cc_swe_dwd._weekend_check(dt.date(2025, 3, 9), WeekEndPolicy.PREV_BANK_DAY)
     assert expected == actual
+
 
 def test_weekend_check_next_business_day_sunday(cc_swe_dwd: CreditCard):
     expected = dt.date(2025, 3, 10)
-    actual = cc_swe_dwd._weekend_check(
-        dt.date(2025, 3, 9), WeekendCheckPolicy.NEXT_BUSINESS_DAY
-    )
+    actual = cc_swe_dwd._weekend_check(dt.date(2025, 3, 9), WeekEndPolicy.NEXT_BANK_DAY)
     assert expected == actual
+
 
 def test_next_payment_before_stmnt(cc: CreditCard):
     expected = dt.date(2025, 3, 3)
-    actual = cc.next_payment(dt.date(2025, 3, 1))
+    actual = cc.next_due_date(dt.date(2025, 3, 1))
     assert expected == actual
 
 
 def test_next_payment_on_stmnt(cc: CreditCard):
     expected = dt.date(2025, 3, 3)
-    actual = cc.next_payment(dt.date(2025, 3, 2))
+    actual = cc.next_due_date(dt.date(2025, 3, 2))
     assert expected == actual
 
 
@@ -90,5 +93,27 @@ def test_next_payment_after_stmnt(cc: CreditCard):
     _input = dt.date(2025, 3, 3)
     expct = dt.date(2025, 4, 3)
 
-    actual = cc.next_payment(_input)
+    actual = cc.next_due_date(_input)
     assert expct == actual
+
+
+def test_stmnt_falls_on_non_existent_day_next_month():
+    cc = CreditCard("bank", "name", "idk", 1, 30)
+    test_purchase_date = dt.date(2025, 1, 31)
+
+    expc_statement_date = dt.date(2025, 2, 28)
+    actual = cc.next_statement(test_purchase_date)
+    assert expc_statement_date == actual
+
+
+def test_dates_on_purchase(cc: CreditCard):
+    installment = CreditCardInstallment(
+        cc,
+        3,
+        dt.date(2025, 3, 2),
+        InstallmentPolicy.ON_PURCHASE_DAY,
+        100.00,
+        InstallmentAmountType.MONTHLY_FIXED,
+    )
+    charge_dates = installment.get_charge_dates()
+    print("edi wow")
