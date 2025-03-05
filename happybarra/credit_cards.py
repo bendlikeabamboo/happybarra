9aae32fc7d27102d8167ff94847230d20f5fd121
Manuel B. <mbalmeo32@gmail.com>
2025-03-05 09:59:36 +0800

    added banks cc and network models, new forms

---
+++ happybarra/credit_cards.py
@@ -0,0 +1,25 @@
+from functools import partial
+import datetime as dt
+from happybarra.models import (
+    CreditCard,
+    DueDateType,
+    CreditCardInstallment,
+    InstallmentPolicy,
+    InstallmentAmountType,
+)
+from happybarra.banks import BPI
+from happybarra.network import VISA
+
+prepop_BPI__MASTERCARD__REWARDS = partial(CreditCard, BPI, "Rewards", VISA)
+
+if __name__ == "__main__":
+    cc = prepop_BPI__MASTERCARD__REWARDS(20, 15)
+    cci = CreditCardInstallment(
+        cc,
+        6,
+        dt.date(2025, 3, 5),
+        InstallmentPolicy.ON_PURCHASE_DAY,
+        500.00,
+        InstallmentAmountType.MONTHLY_FIXED,
+    )
+    print(cci.get_charge_dates())
